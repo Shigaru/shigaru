85b10b18f1cca9efa160144b8e83490ca4940006
murcialito <murcialito@gmail.com>
2012-11-03 22:58:14 +0100

    ddd

--- plugins/hwdvs-thirdparty/youtube.php
+++ plugins/hwdvs-thirdparty/youtube.php
@@ -130,6 +130,7 @@ class hwd_vs_tp_YoutubeCom
 			curl_setopt($curl_handle,CURLOPT_CONNECTTIMEOUT,2);
 			curl_setopt($curl_handle,CURLOPT_RETURNTRANSFER,1);
 			$buffer = curl_exec($curl_handle);
+			var_dump($buffer);
 			curl_close($curl_handle);
 
 			if (!empty($buffer))
