b6fb820088d8cc8fba05365a21cae04fe1544733
murcialito <murcialito@gmail.com>
2013-11-08 18:32:57 +0100

    .dsds

--- components/com_sphinxsearch/autocomplete/ajax_suggest.php
+++ components/com_sphinxsearch/autocomplete/ajax_suggest.php
@@ -8,7 +8,7 @@ $autoCompleteSearch = new SphinxAutoComplete();
 $results = $autoCompleteSearch->search($q);
 $counter = 0;
 foreach ($results['matches'] as $key => $value) {
-    echo '<div class="clearfix"><div class="fleft w85">'.$results['excerpts'][$counter].'</div><img width="50" class="fleft" src="/shigaru/hwdvideos/thumbs/tp-'.$key.'.jpg"/></div>'.'|'.$results['excerpts'][$counter]. "\n";
+    echo '<div class="clearfix"><div class="fleft w85">'.$results['excerpts'][$counter].'</div><img width="50" class="fleft" src="/hwdvideos/thumbs/tp-'.$key.'.jpg"/></div>'.'|'.$results['excerpts'][$counter]. "\n";
     /*echo '<pre>';
     var_dump($value);
     echo '</pre>';*/
