f4a9daff79ae381d4eeeac1bd43de3705a29d26a
murcialito <murcialito@gmail.com>
2014-04-25 19:55:57 +0200

    a to z changes, maps

--- components/com_hwdvideoshare/models/ajaxsuite.php
+++ components/com_hwdvideoshare/models/ajaxsuite.php
@@ -356,17 +356,15 @@ class hwd_vs_ajax
 		}
 	
 	function getLocationEvents(){
-		  //$SongKickURL = 'http://api.hostip.info/get_json.php?ip=12.215.42.19&position=true';
-		  $SongKickURL = 'http://api.hostip.info/get_json.php?ip='.urlencode($_SERVER['REMOTE_ADDR']).'&position=true';
+		  $SongKickURL = 'http://api.songkick.com/api/3.0/events.json?location=ip:'.urlencode($_SERVER['REMOTE_ADDR']).'&apikey=EKOiqRoWeX1ZQ9vZ';
+		  //$SongKickURL = 'http://api.songkick.com/api/3.0/events.json?location=ip:2.136.83.249&apikey=EKOiqRoWeX1ZQ9vZ';
 		  $gsJson 		= file_get_contents($SongKickURL,0,null,null);
-		  $songResults 	= $gsJson;
 		  $oJsongResults = json_decode($gsJson);
-		  if($oJsongResults->country_code != 'XX'){
-				  $SongKickURL = 'http://api.songkick.com/api/3.0/search/locations.json?location=geo:'.$oJsongResults->lat.','.$oJsongResults->lng.'&apikey=EKOiqRoWeX1ZQ9vZ';
-				  $gsJson 		= file_get_contents($SongKickURL,0,null,null);
-				  $songResults 	= $gsJson;
-				  $oJsongResults = json_decode($gsJson);
-			  }
+		  $songResults 	= $gsJson;
+		  /*if($oJsongResults[0][4]){
+				  $SongKickURL = 'http://api.songkick.com/api/3.0/search/locations.json?location=geo:'.$oJsongResults[0][4].','.$oJsongResults[0][5].'&apikey=EKOiqRoWeX1ZQ9vZ';
+				  $songResults = file_get_contents($SongKickURL,0,null,null);
+			  }*/
 		 return $songResults;
 		}
 		
@@ -395,7 +393,7 @@ class hwd_vs_ajax
 			$songResults 	= hwd_vs_ajax::getLocationEvents();
 			}	  
 		/*echo '<pre>';
-		var_dump($oJsongResults);
+		var_dump(json_decode($songResults));
 		echo '</pre>';
 		//var_dump($songResults);*/
 		echo $songResults;
--- plugins/hwdvs-template/shigaru/js/atoz.js
+++ plugins/hwdvs-template/shigaru/js/atoz.js
@@ -11,12 +11,14 @@ jQuery(document).ready(function () {
             success: function (data) {
 				var oBandEventsDiv = jQuery("#bandevents");
 				oBandEventsDiv.empty().show(500,function(){
-					if(data.resultsPage.results.location){
+					if(data.resultsPage.status == 'ok' && data.resultsPage.results.event){
 						jQuery( "<div />" ).attr('id','map-canvas').css({'width':'250px','height':'250px'}).appendTo(oBandEventsDiv);
-						initialPoint = data.resultsPage.results.location[0].city;
-						markers = data.resultsPage.results.location;	
+						initialPoint = data.resultsPage.results.event[0].location;
+						markers = data.resultsPage.results.event;	
 						loadScript();		
-					}					
+					}else{
+						jQuery( '#bandevents').hide().prev().hide();
+						}					
 				});
             }
         });
@@ -68,15 +70,40 @@ function loadScript() {
   document.body.appendChild(script);
 }
 
+function addInfoWindow(marker, oInfowindow) {
+	google.maps.event.addListener(marker, 'click', function () {
+		closeAllInfoWindows();
+		oInfowindow.open(map, marker);
+		infoWindows.push(oInfowindow);
+	});
+}
+var infoWindows = [];
+function closeAllInfoWindows() {
+  for (var i=0;i<infoWindows.length;i++) {
+     infoWindows[i].close();
+  }
+}
+
 function addMarkers(){
-	
 	for (var i = 0; i < markers.length; i++) {
             var marker = new google.maps.Marker({
-                position: new google.maps.LatLng(markers[i].city.lat,markers[i].city.lng),
+                position: new google.maps.LatLng(markers[i].location.lat,markers[i].location.lng),
                 map: map,
-                title: markers[i].city.displayName+' ('+markers[i].city.country.displayName+') ',
+                title: markers[i].location.city+' ('+markers[i].displayName+') ',
                 zIndex: i
             });
+			var oInfowindow = new google.maps.InfoWindow({
+			  content: '<div id="content">'+
+			  '<div id="siteNotice">'+
+			  '</div>'+
+			  '<h1 id="firstHeading" class="firstHeading">'+markers[i].location.city	+'</h1>'+
+			  '<div id="bodyContent">'+
+			  markers[i].displayName +
+			  '</div>'+
+			  '</div>'
+			});
+             addInfoWindow(marker, oInfowindow);
+			  
         }
 	
 	}
