021be56f2cbb0ffd9a3829afc3edab6ee08d9c72
murcialito <murcialito@gmail.com>
2014-05-27 18:50:45 +0200

    adding language to channel urls

--- components/com_shigarututor/controller.php
+++ components/com_shigarututor/controller.php
@@ -65,13 +65,14 @@ class ShigarututorController extends JController
 	
 	function gotovideo(){
 		  require_once(JPATH_SITE.DS.'components'.DS.'com_shigarututor'.DS.'shigarututor.class.php');
+		  $lang = JFactory::getLanguage();	
 		  $f = new shigarututorTools;
 		  $url = '';
 		  $video_id = Jrequest::getVar( 'video_id', '' );
 		  $oShigaruVideo = $f->getVideoByExternalId($video_id);
 		  $app = & JFactory::getApplication();
 		  if($oShigaruVideo)
-			$url = 'index.php?option=com_hwdvideoshare&task=viewvideo&video_id='.$oShigaruVideo->id;
+			$url = 'index.php?option=com_hwdvideoshare&task=viewvideo&video_id='.$oShigaruVideo->id.'&lang='.substr($lang->getTag(),0,2);
 			else{
 					$url = htmlspecialchars($_SERVER['HTTP_REFERER']);
 					JError::raiseNotice( 100, JText::_('NOTYETINSHIGARU'));
