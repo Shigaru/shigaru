f1ab0a739635011cc09bc30b6012dd57a63c3cc1
murcialito <murcialito@gmail.com>
2014-06-05 14:04:33 +0200

    .

--- components/com_shigaruhome/views/shigaruhome/tmpl/js/shigaruhome.js
+++ components/com_shigaruhome/views/shigaruhome/tmpl/js/shigaruhome.js
@@ -41,6 +41,7 @@ jQuery(document).ready(function() {
        var alt = data[i].title;
        var pic = null;
        var meass = ''; 
+       var oCount = addSpecificData(opts.oContentItemId.substring(opts.oContentItemId.length-3,opts.oContentItemId.length), data[i]);
        alt = alt.replace(/[^a-z0-9\s]/gi, '').replace(/[_\s]/g, ' ');
        if(img !=''){
 			if(img.indexOf('http') < 0)
@@ -50,7 +51,10 @@ jQuery(document).ready(function() {
 			pic = '<img '+meass+' class="mright6" src="'+img+'" alt="'+alt+'" title="'+alt+'" />'	
 				}else if(data[i].thumbnail =='')
 					pic = '<img class="mright6" width="20" height="20" src="templates/rhuk_milkyway/images/vinyl-icon.png" alt="'+alt+'" title="'+alt+'" />';					 
-       content += '<div><a href="index.php?option=com_hwdvideoshare&task=viewvideo&video_id='+data[i].id+'&lang='+oLanguage+'" title="' +alt+ '">'+pic+ '</a></div>'
+       content += '<div><div><a href="index.php?option=com_hwdvideoshare&task=viewvideo&video_id='+data[i].id+'&lang='+oLanguage+'" title="' +alt+ '">'+pic+ '</a><div class="mright12"><span class="f60 mright24 videotime">'+data[i].video_length+'</span></div></div>'
+       content += '<div class="f13px mtop2 pad2"><h6><a href="index.php?option=com_hwdvideoshare&task=viewvideo&video_id='+data[i].id+'&lang='+oLanguage+'" title="' +alt+ '">'+data[i].title+'</a></h6></div>'
+       content += '<div class="f60">'+data[i].date_uploaded+'</div>'
+       content += '</div>'
     }
     jQuery(opts.oContentItemId).html(content);
   }
@@ -61,8 +65,22 @@ jQuery(document).ready(function() {
 			jsonPath : opts.paramUrl,
 			jsonSuccess : function(data){customDataSuccess(data,opts.oContentItemId)},
 			items : opts.items,
+			lazyLoad : opts.lazyLoad,
 			navigation : opts.navigation
 		  });
+	  }
+	  
+  function addSpecificData(paramType, paramData){
+	  var oSpecificData = '';
+		switch(paramType){
+			case 'recent':
+							oSpecificData +='';
+							break;
+			default:
+							oSpecificData +='<div>'+paramData+'</div>';
+							break;
+			
+			}
 	  }
 	  
   function addSortingControls(){
@@ -80,8 +98,9 @@ jQuery(document).ready(function() {
   }
 
   jQuery.fn.shigaruHome.defaults = {
-	items : 6,
+	items : 5,
 	navigation : true,
+	lazyLoad : false,
 	paramUrl : 'index.php?option=com_shigaruhome&task=getvideolist&list_type=liked&format=raw&lang=en',
 	oContentItemId : '#shigaruowl'
   }
